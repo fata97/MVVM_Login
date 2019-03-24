0e82e53221b819171507442cffd843baeb9f7a9f
fata97 <fatahasan97@gmail.com>
2019-03-24 17:02:35 +0700

    MVVM Done

--- app/src/main/java/com/example/mvvmlogin/view/MainActivity.java
+++ app/src/main/java/com/example/mvvmlogin/view/MainActivity.java
@@ -2,14 +2,38 @@ package com.example.mvvmlogin.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.mvvmlogin.R;
+import com.example.mvvmlogin.viewmodel.UserViewModel;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText inputEmail, inputPassword;
+    private Button Login;
+    private UserViewModel userViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputEmail = findViewById(R.id.email);
+        inputPassword = findViewById(R.id.password);
+        Login = findViewById(R.id.btnLogin);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("test",inputEmail.getText().toString());
+                userViewModel.updateModel(inputEmail.getText().toString(), inputPassword.getText().toString());
+                userViewModel.validLogin();
+                //Toast.makeText(MainActivity.this, inputEmail.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/mvvmlogin/viewmodel/UserViewModel.java
+++ app/src/main/java/com/example/mvvmlogin/viewmodel/UserViewModel.java
@@ -1,4 +1,50 @@
 package com.example.mvvmlogin.viewmodel;
 
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.mvvmlogin.model.User;
+
 public class UserViewModel {
+    public final static String TOAST_MESSAGE_SUCCESS = "Login Success";
+    public final static String TOAST_MESSAGE_FAILED = "Login Failed";
+    private User user;
+    private Context context;
+
+    public UserViewModel(Context context, User user){
+        this.user = user;
+        this.context = context;
+    }
+
+    public void updateModel(String email, String password){
+        user.setEmail(email);
+        user.setPassword(password);
+
+        //Log.d("hiks1",user.getEmail());
+        //Log.d("hiks2",user.email);
+        Log.d("hiks3",email);
+        //Toast.makeText(context, user.getEmail(), Toast.LENGTH_SHORT).show();
+    }
+
+    public void validLogin(){
+        boolean isValid = true;
+
+        if (TextUtils.isEmpty(user.getEmail()))
+            isValid = false;
+
+        if (TextUtils.isEmpty(user.getPassword()))
+            isValid = false;
+
+        if (isValid)
+            showToast(TOAST_MESSAGE_SUCCESS);
+        else
+            showToast(TOAST_MESSAGE_FAILED);
+    }
+
+    private void showToast(String messageToast){
+        Toast.makeText(context, messageToast, Toast.LENGTH_SHORT).show();
+    }
+
 }
